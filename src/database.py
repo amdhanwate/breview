e6cb2ef33fcfad6a7dfaa10c35458286d33a7c31
Abhijeet Dhanwate <62416129+amdhanwate@users.noreply.github.com>
2023-11-10 17:38:25 +0530

    use hosted db

--- src/database.py
+++ src/database.py
@@ -2,9 +2,10 @@ import psycopg2
 
 def get_db_connection():
     conn = psycopg2.connect(
-        dbname="brewery",
-        user="abhi",
-        password="password",
-        host="localhost",
+        dbname="breview",
+        user="amdhanwate",
+        password="VZqrO0bj3LYD",
+        host="ep-cool-mouse-74251756.ap-southeast-1.aws.neon.tech",
+        port=5432
     )
     return conn
